2eaf45c5b77caa708c9bd106cdf1b207404c3932
DhavitG <gdhavit@gmail.com>
2025-04-07 23:30:36 +0530

    Greetings

--- Hello.java
+++ Hello.java
@@ -1,5 +1,16 @@
 public class Hello {
+  private static String greeting = "Hello";
+
+  public static void greetUser(String name) {
+      System.out.println(greeting + ", " + name + "!");
+  }
+
   public static void main(String[] args) {
-      System.out.println("Hello, World!");
+      if (args.length > 0) {
+          greetUser(args[0]);
+      } else {
+          System.out.println("Hello, World!");
+          System.out.println("Tip: Run again with your name as an argument :)");
+      }
   }
 }
