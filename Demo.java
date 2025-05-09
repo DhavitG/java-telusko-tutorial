1eeba5292b9ab898712b56981bdae5c3d7337466
DhavitG <gdhavit@gmail.com>
2025-05-09 11:51:48 +0530

    Conditionals

--- Demo.java
+++ Demo.java
@@ -1,9 +1,23 @@
 public class Demo {
   public static void main(String[] args) {
-    double x = 5;
-    int y = 5;
+    int x = 8;
+    int y = 7;
+    int z = 6;
 
-    boolean result = x == y;
-    System.out.println(result);
+    if (x>y && x>z) {
+      System.out.println(x);
+    }
+    else if(y > z){
+      System.out.println(y);
+    }
+    else {
+      System.out.println(z);
+    }
+
+    int m = 9;
+    int result = m > 10 ? 1 : 0;
+  
+    System.out.println("Result is " + result + " which is a " + (result == 0 ? "false value" : "true value"));
   }
+
 }
