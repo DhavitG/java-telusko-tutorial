dfecfbe373a35e8f91aecd7eb3ff52bb67a91660
DhavitG <gdhavit@gmail.com>
2025-06-05 01:05:55 +0530

    Custom Class

--- Demo.java
+++ Demo.java
@@ -1,4 +1,15 @@
+class Calculator {
+  int num1 = 4;
+  int num2 = 5;
+
+  public int add(int num1, int num2) {
+    int r = num1 + num2;
+    return r;
+  }
+}
+
 public class Demo {
+  /* 
   public static void main(String[] args) {
     int x = 8;
     int y = 7;
@@ -19,5 +30,11 @@ public class Demo {
   
     System.out.println("Result is " + result + " which is a " + (result == 0 ? "false value" : "true value"));
   }
+  */
 
+  public static void main(String[] args) {
+    Calculator calc = new Calculator();
+    int result = calc.add(calc.num1, calc.num2);
+    System.out.println(result);
+  }
 }
