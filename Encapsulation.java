ad8cb155456331924e0e51fb14cbce28e5c63d1d
DhavitG <gdhavit@gmail.com>
2025-06-10 04:22:49 +0530

    Encapsulation (private)

---
+++ Encapsulation.java
@@ -0,0 +1,35 @@
+class Human {
+  // private int age = 13;
+  // private String name = "Dhavit";
+
+  private int age;
+  private String name;
+
+  public void setAge(int a) {
+    age = a;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setName(String str) {
+    name = str;
+  }
+  public String getName() {
+    return name;
+  }
+}
+
+public class Encapsulation {
+  public static void main(String a[]) {
+    Human obj = new Human();
+    // obj.age = 13;
+    // obj.name = "dhavit";
+
+    obj.setAge(13);
+    obj.setName("Dhavit");
+
+    System.out.println(obj.getName() + " : " + obj.getAge() + " years old.");
+  }
+}
