df7a2ff7956713610320724a39d28e07f8f9062f
Dhavit Gandhi <gdhavit@gmail.com>
2025-07-10 22:22:40 +0530

    Usage of collection interface

---
+++ Collections.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Collections {
+  public static void main(String a[]) {
+
+    Collection<Integer> nums = new ArrayList<Integer>();
+    nums.add(8);
+    nums.add(4);
+    nums.add(1);
+
+    System.out.println(nums);
+  }
+}
